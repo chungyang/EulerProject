2a200bc1d295bdb3feec13c20508eb1fc2620f8b
chung yang <chung.yang@engineer.uconn.edu>
2016-09-26 06:05:50 -0400

    Problem 7 has 20% improvement and solved problem 10

---
+++ Problem 10/src/SummationOfPrimes.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by chungyang on 9/23/16.
+ */
+public class SummationOfPrimes {
+
+    public static void main(String[] args) {
+
+        int n = 2000000;
+        boolean isprime[] = new boolean[n + 1];
+        Arrays.fill(isprime, true);
+        isprime[2] = true;
+        isprime[3] = true;
+        for (int i = 4; i < n; i += 2) isprime[i] = false;
+        for (int i = 3; i < n; i += 2) {
+            for (int j = i + i; j < n; j += i) {
+                isprime[j] = false;
+            }
+        }
+        long sum = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isprime[i]) sum += i;
+        }
+        System.out.println(sum);
+
+    }
+
+
+}
+
+
--- Problem 7/TenThousandstPrime.java
+++ Problem 7/TenThousandstPrime.java
@@ -1,4 +1,7 @@
+//Find the 10001st prime number
+
 import java.util.*;
+import java.math.BigInteger;
 
 public class TenThousandstPrime{
    
@@ -9,6 +12,7 @@ public class TenThousandstPrime{
        
 	while(true){
 	    
+	    if(isProbablePrime(new BigInteger(Integer.toString(number)),10)){
 	    if(isPrime(number)){
 
 		if(nthPrime == Integer.parseInt(args[0])){
@@ -18,6 +22,7 @@ public class TenThousandstPrime{
 		
 		nthPrime++;
 	    }
+	}
 
 	    number += 2;
 	   
@@ -44,4 +49,46 @@ public class TenThousandstPrime{
 	    return false;
 	}
     }
+
+    private static final BigInteger ZERO = BigInteger.ZERO;
+	private static final BigInteger ONE = BigInteger.ONE;
+	private static final BigInteger TWO = new BigInteger("2");
+	private static final BigInteger THREE = new BigInteger("3");
+
+	public static boolean isProbablePrime(BigInteger n, int k) {
+		if (n.compareTo(THREE) < 0)
+			return true;
+		int s = 0;
+		BigInteger d = n.subtract(ONE);
+		while (d.mod(TWO).equals(ZERO)) {
+			s++;
+			d = d.divide(TWO);
+		}
+		for (int i = 0; i < k; i++) {
+			BigInteger a = uniformRandom(TWO, n.subtract(ONE));
+			BigInteger x = a.modPow(d, n);
+			if (x.equals(ONE) || x.equals(n.subtract(ONE)))
+				continue;
+			int r = 1;
+			for (; r < s; r++) {
+				x = x.modPow(TWO, n);
+				if (x.equals(ONE))
+					return false;
+				if (x.equals(n.subtract(ONE)))
+					break;
+			}
+			if (r == s) // None of the steps made x equal n-1.
+				return false;
+		}
+		return true;
+	}
+
+	private static BigInteger uniformRandom(BigInteger bottom, BigInteger top) {
+		Random rnd = new Random();
+		BigInteger res;
+		do {
+			res = new BigInteger(top.bitLength(), rnd);
+		} while (res.compareTo(bottom) < 0 || res.compareTo(top) > 0);
+		return res;
+	}
 }
